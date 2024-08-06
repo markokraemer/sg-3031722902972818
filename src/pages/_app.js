bffbe65c846273604dfd76ceb2e23b7082b4b450
SoftGen <admin@softgen.ai>
2024-08-06 00:22:02 +0000

    SOFTGEN MAGIC

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,28 @@
 import '@/styles/globals.css';
+import { useEffect } from 'react';
+
+function ErrorFallback({ error }) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+    </div>
+  )
+}
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    console.log('MyApp mounted');
+    return () => console.log('MyApp unmounted');
+  }, []);
+
   console.log('MyApp is rendering');
-  return <Component {...pageProps} />;
+
+  return (
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  );
 }
 
 export default MyApp;
